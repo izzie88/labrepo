bca8a8e8005f7da313e1ec7d5856936e100dced5
dirivbogbe <dirivbogbe@teamapt.com>
2019-10-29 08:53:40 +0100

    added changes to struct

--- definedtype.go
+++ definedtype.go
@@ -7,8 +7,20 @@ type person struct{
     Sex  string
 }
 
+type Vertex struct {
+    X, Y int
+}
+
+var (
+    p = Vertex{1, 2}  // has type Vertex
+    q = &Vertex{1, 2} // has type *Vertex
+    r = Vertex{X: 1}  // Y:0 is implicit
+    s = Vertex{}      // X:0 and Y:0
+)
+
 func main(){
-    p := person{Name:"David", Age:30, Sex: "Male"}
+    m := person{Name:"David", Age:30, Sex: "Male"}
     fmt.Println(p)
+    fmt.Println(p, q, r, s)
 }
 
